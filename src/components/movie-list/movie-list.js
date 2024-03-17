6f1ad05ccbc3d0c00d8b3785b6070c9563efa85a
SunnatUmurqulov <umurqulovsunnat3@gmail.com>
2024-03-17 17:12:45 +0500

    full commit

---
+++ src/components/movie-list/movie-list.js
@@ -0,0 +1,16 @@
+import MovieListItem from "../movie-list-item/movie-list-item"
+import "./movie-list.css"
+
+const MovieList = () => {
+  return (
+    <div className="movie-list">
+        <MovieListItem/>
+        <MovieListItem/>
+        <MovieListItem/>
+        <MovieListItem/>
+        <MovieListItem/>
+    </div>
+  )
+}
+
+export default MovieList
